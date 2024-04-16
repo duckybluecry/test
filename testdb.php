0d7753e9dec5467bdbeda58b685bb31b3a646db2
duckybluecry <167198623+duckybluecry@users.noreply.github.com>
2024-04-16 14:29:25 +0200

    Add files via upload

---
+++ testdb.php
@@ -0,0 +1,42 @@
+<?php
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+
+
+
+$servername = "localhost";
+$database = "xx";
+$username = "xx";
+$password = "xx";
+
+
+
+//$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+////////$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to Connect to '$dbhost'");
+$link = mysqli_connect($servername, $username, $password, $database) or die("Unable to Connect to '$servername'");
+
+
+
+mysqli_select_db($link, $database) or die("Could not open the db '$database'");
+
+$test_query = "SHOW TABLES FROM $database";
+$result = mysqli_query($link, $test_query);
+
+$tblCnt = 0;
+while($tbl = mysqli_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
+  
+}
+
+if (!$tblCnt) {
+  echo "There are no tables<br />\n";
+} else {
+  echo "There are $tblCnt tables<br />\n";
+   echo "ok";
+  
+}
+mysqli_close($link);
+
+
+?>
